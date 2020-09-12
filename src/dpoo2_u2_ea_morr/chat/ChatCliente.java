22827ac227dbb7b7ff8a680fa66501c2bf599bdb
alanheraclio <alanheraclio@gmail.com>
2020-09-12 17:50:27 -0500

    chat commented

--- src/dpoo2_u2_ea_morr/chat/ChatCliente.java
+++ src/dpoo2_u2_ea_morr/chat/ChatCliente.java
@@ -16,6 +16,7 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 public class ChatCliente {
+    //SE CREAN LOS ELEMENTOS PARA CONSTRUIR LA VENTANA DEL CLIENTE
     JFrame ventana = null;
     JButton boton_enviar = null;
     JTextField mensaje_field = null;
@@ -24,11 +25,13 @@ public class ChatCliente {
     JPanel boton_panel = null;
     JScrollPane scroll = null;
     
+    //SE CREAN LOS ELEMTOS PARA LA COMUNICACION POR SOCKETS
     Socket socket = null;
     BufferedReader lector = null;
     PrintWriter escritor = null;
     
     public ChatCliente() {
+        //SE INICIALIZAN TODOS LOS ELEMENTOS EN EL CONSTRUCTOR
         ventana = new JFrame("Cliente");
         boton_enviar = new JButton("Enviar");
         mensaje_field = new JTextField(5);
@@ -48,51 +51,65 @@ public class ChatCliente {
         ventana.setVisible(true);
         ventana.setResizable(false); 
         
+        //SE LLAMA LA FUNCION QUE CONTIENE LOS HILOS DE LA COMUNICACION DEL CHAT
         correrHilos();
     } 
     
+    //CLASE QUE LLAMA LAS FUNCIONES DE LEER Y EXCRIBIR EN EL CHAT
     public void correrHilos(){
+        //SE CREA UN HILO PARA CORRER LOS OTROS HILOS DENTRO DE EL
         System.out.println("correr hilos cliente corriendo...");
         Thread main = new Thread(new Runnable() {
             public void run() {
+                //SE INTENTA COMUNICAR AL CANAL DE COMUNICACION DEL SERVIDOR
                 try {
+                    //SE CONECTA AL PUERTO DEL CANAL DE COMUNACION QUE ABRIO EL SERVIDOR
                     socket = new Socket("localhost",9000);
+                    //SE LLAMA A LOS METODOS LEER Y ESCRIBIR
                     leerMensaje();
                     escribirMensaje();
-                    
+                 //EN CASO DE ERROR SE MUESTRA EL MENSAJE   
                 } catch (Exception e) {
                     System.out.println("Error al correr socket en el cliente"+e);
                 }
             }
         });
+        //SE ARRANCA EL HILO
         main.start();
     }
     
+    //METODO QUE CONTIENE EL HILO QUE OBTENDR ALOS MENSAJES
     public void leerMensaje(){
+        //SE CREA UN HILO QUE MANTENDR ALA LECTURA DE LOS MENSAJES
         Thread leer_hilo = new Thread(new Runnable(){
             public void run(){
-                
+                //SE INTENTA OBTENER EL MENSAJE Y SE MUESTRA EN LA VENTANA
                 try {
                 lector = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                 while(true){
                     String mensaje_recibido = lector.readLine();
                     mensaje_area.append("Servidor dice: "+mensaje_recibido+"\n");
                 }
-
+                //EN CASO DE ERROR SE MUESTRA EL MENSAJE
                 } catch (Exception e) {
                     System.out.println("chat error al leer"+e);
                 }
             
             }
         });
+        //SE ARRANCA EL HILO LEER
         leer_hilo.start();
     }
     
+    //METODO PARA ENVIAR MENSAJES
     public void escribirMensaje(){
+        //SE CREA UN HILO PARA ENVIAR MENSAJES
         Thread escribir_hilo = new Thread(new Runnable() {
             public void run() {
                 try {
+                    //SE INTENTA ENVIAR EL MENSAJE
                     escritor = new PrintWriter(socket.getOutputStream(),true);
+                    //SE ASIGNA LA ACCION AL BOTON ENVIAR
                     boton_enviar.addActionListener(new ActionListener(){
                         public void actionPerformed(ActionEvent e){
                             String mensaje = mensaje_field.getText();
@@ -102,11 +119,13 @@ public class ChatCliente {
                             System.out.println("cliente boton enviar clicked");
                         }            
                     });
+                    //EN CASO DE ERROR SE ENVIA EL MENSAJE
                 } catch (Exception e) {
                     System.out.println("chat error al escribir"+e);
                 }
             }
         });
+        //SE ARRNACA EL HILO PARA ENVIAR EL MENSAJE
         escribir_hilo.start();
     }
     
--- src/dpoo2_u2_ea_morr/chat/ChatServidor.java
+++ src/dpoo2_u2_ea_morr/chat/ChatServidor.java
@@ -17,7 +17,7 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 public class ChatServidor {
-    
+    //SE CREAN LOS ELEMENTOS PARA CONSTRUIR LA VENTANA DEL SERVIDOR
     JFrame ventana = null;
     JButton boton_enviar = null;
     JTextField mensaje_field = null;
@@ -26,12 +26,14 @@ public class ChatServidor {
     JPanel boton_panel = null;
     JScrollPane scroll = null;
     
+    //SE CREAN LOS ELEMTOS PARA LA COMUNICACION POR SOCKETS
     ServerSocket servidor = null;
     Socket socket = null;
     BufferedReader lector = null;
     PrintWriter escritor = null;
     
     public ChatServidor() {
+        //SE INICIALIZAN TODOS LOS ELEMENTOS EN EL CONSTRUCTOR
         ventana = new JFrame("Servidor");
         boton_enviar = new JButton("Enviar");
         mensaje_field = new JTextField(5);
@@ -51,58 +53,66 @@ public class ChatServidor {
         ventana.setVisible(true);
         ventana.setResizable(false); 
         
+        //SE LLAMA LA FUNCION QUE CONTIENE LOS HILOS DE LA COMUNICACION DEL CHAT
         correrHilos();        
 
     }   
-    
+    //CLASE QUE LLAMA LAS FUNCIONES DE LEER Y EXCRIBIR EN EL CHAT
     public void correrHilos(){
-        System.out.println("correr hilos server corriendo...");
-
+            //SE CREA UN HILO PARA CORRER LOS OTROS HILOS DENTRO DE EL
             Thread main = new Thread(new Runnable() {
             public void run() {
+                    //SE ABRE EL CANAL DE COMUNICACION
                      try {
                         servidor = new ServerSocket(9000);
+                        //SE HACE UN CICLO INFINITO PARA QUE NO SE TERMINE LA COMUNICACION
                         while(true){
-                            System.out.println("hilo main corriendo... ");
+                            //SE ACEPTA LA COMUNICACION EN EL SOCKET Y SE LLAMA A LOS METODOS LEER Y ESCRIBIR
                             socket = servidor.accept();
                             leerMensaje();
                             escribirMensaje();
                         }
+                        //EN CASO DE ERROR SE MUESTRA EL MENSAJE
                     } catch (Exception e) {
                         System.out.println("error al conectarse al puerto: "+e);
                     }
             }
         });
+        //SE ARRANCA EL HILO
         main.start();   
   }
     
+    //METODO QUE CONTIENE EL HILO QUE OBTENDR ALOS MENSAJES
     public void leerMensaje(){
+        //SE CREA UN HILO QUE MANTENDR ALA LECTURA DE LOS MENSAJES
         Thread leer_hilo = new Thread(new Runnable(){
             public void run(){
-                System.out.println("hilo leer corriendo... ");
+                //SE INTENTA OBTENER EL MENSAJE Y SE MUESTRA EN LA VENTANA
                 try {
-                    System.out.println("hilo leer try corriendo... ");
                     lector = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                     while(true){
                         String mensaje_recibido = lector.readLine();
                         mensaje_area.append("Cliente dice: "+mensaje_recibido+"\n");
                     }
-
+                  //EN CASO DE ERROR SE MUESTRA EL MENSAJE
                 } catch (Exception e) {
                     System.out.println("chat error al leer"+e);
                 }
             }
         });
+        //SE ARRANCA EL HILO LEER
         leer_hilo.start();
     }
-        
+     
+    //METODO PARA ENVIAR MENSAJES
     public void escribirMensaje(){
+        //SE CREA UN HILO PARA ENVIAR MENSAJES
         Thread escribir_hilo = new Thread(new Runnable() {
             public void run() {
-                System.out.println("hilo escribir corriendo... ");
                 try {
-                    System.out.println("hilo escribir try corriendo... ");
+                    //SE INTENTA ENVIAR EL MENSAJE
                     escritor = new PrintWriter(socket.getOutputStream(),true);
+                    //SE ASIGNA LA ACCION AL BOTON ENVIAR
                     boton_enviar.addActionListener(new ActionListener(){
                         public void actionPerformed(ActionEvent e){
                             String mensaje = mensaje_field.getText();
@@ -112,11 +122,13 @@ public class ChatServidor {
                             System.out.println("servidor boton enviar clicked");
                         }            
                     });
+                  //EN CASO DE ERROR SE ENVIA EL MENSAJE
                 } catch (Exception e) {
                     System.out.println("chat error al escribir"+e);
                 }
             }
         });
+        //SE ARRNACA EL HILO PARA ENVIAR EL MENSAJE
         escribir_hilo.start();
     }
         
